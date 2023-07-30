e902b191b7d6c4c63b92dfc38feadac99dd06eca
Simo Leone <simo.leone@iki.fi>
2023-07-30 23:42:18 +0300

    Check Gemfile for sorbet gem and completness, support multiple modules properly

--- src/main/kotlin/im/simo/rubymine/sorbetlsp/SorbetLspServerSupportProvider.kt
+++ src/main/kotlin/im/simo/rubymine/sorbetlsp/SorbetLspServerSupportProvider.kt
@@ -2,46 +2,76 @@ package im.simo.rubymine.sorbetlsp
 
 import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.execution.process.OSProcessHandler
+import com.intellij.openapi.module.ModuleUtilCore
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.project.modules
-import com.intellij.openapi.roots.ModuleRootManager
+import com.intellij.openapi.project.rootManager
+import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.platform.lsp.api.LspServerDescriptor
 import com.intellij.platform.lsp.api.LspServerSupportProvider
-import com.intellij.platform.lsp.api.ProjectWideLspServerDescriptor
 import org.jetbrains.plugins.ruby.gem.RubyGemExecutionContext
+import org.jetbrains.plugins.ruby.gem.bundler.BundlerGemInfrastructure
+import org.jetbrains.plugins.ruby.gem.util.BundlerUtil
 import org.jetbrains.plugins.ruby.ruby.lang.RubyFileType
+import org.jetbrains.plugins.ruby.ruby.sdk.RubySdkUtil
 
+@Suppress("UnstableApiUsage")
 class SorbetLspServerSupportProvider : LspServerSupportProvider {
-    override fun fileOpened(project: Project, file: VirtualFile, serverStarter: LspServerSupportProvider.LspServerStarter) {
-        if (file.fileType == RubyFileType.RUBY) {
-            serverStarter.ensureServerStarted(SorbetLspServerDescriptor(project))
-        }
+  override fun fileOpened(
+    project: Project, file: VirtualFile, serverStarter: LspServerSupportProvider.LspServerStarter
+  ) {
+    if (file.fileType == RubyFileType.RUBY) {
+      val lspServerDescriptor = SorbetLspServerDescriptor.tryCreate(project, file) ?: return
+      serverStarter.ensureServerStarted(lspServerDescriptor)
     }
+  }
 }
 
-private class SorbetLspServerDescriptor(project: Project) : ProjectWideLspServerDescriptor(project, "Sorbet") {
-    override fun isSupportedFile(file: VirtualFile) = file.fileType == RubyFileType.RUBY
+@Suppress("UnstableApiUsage")
+private class SorbetLspServerDescriptor(
+  val executionContext: RubyGemExecutionContext, project: Project, roots: Array<out VirtualFile>
+) : LspServerDescriptor(project, "Sorbet", *roots) {
+  override fun isSupportedFile(file: VirtualFile) = file.fileType == RubyFileType.RUBY
 
-    override fun startServerProcess(): OSProcessHandler {
-        val module = project.modules[0]
-        val mRoot = ModuleRootManager.getInstance(module)
-        val sdk = mRoot.sdk!!
-        val rubyGemExecutionContext = RubyGemExecutionContext.create(sdk, "sorbet")
-                .withModule(module)
-                .withWorkingDirPath(project.basePath)
-                .withGemScriptName("srb")
-                .withArguments("tc", "--lsp")
+  override fun startServerProcess(): OSProcessHandler {
+    val processHandler = executionContext.withWorkingDirPath(project.basePath).withGemScriptName(GEM_SCRIPT_NAME)
+      .withArguments(GEM_SCRIPT_ARGS).toRubyScriptExecutionContext()!!.createProcessHandler()
+    if (processHandler !is OSProcessHandler) {
+      throw RuntimeException("hmm... RubyProcessHandler wasn't an OSProcessHandler.")
+    }
+    LOG.warn(processHandler.toString())
+    return processHandler
+  }
 
-        val rubyScriptExecutionContext = rubyGemExecutionContext.toRubyScriptExecutionContext()!!
-        val processHandler = rubyScriptExecutionContext.createProcessHandler()
-        if (processHandler !is OSProcessHandler) {
-            throw RuntimeException("hmm... RubyProcessHandler wasn't an OSProcessHandler.")
+  override fun createCommandLine(): GeneralCommandLine {
+    throw NotImplementedError("startServerProcess is implemented instead.")
+  }
+
+  companion object {
+    private const val GEM_NAME = "sorbet"
+    private const val GEM_SCRIPT_NAME = "srb"
+    private val GEM_SCRIPT_ARGS = listOf("tc", "--lsp")
+
+    fun createGemExecutionContext(sdk: Sdk, project: Project, file: VirtualFile): RubyGemExecutionContext? {
+      val module = ModuleUtilCore.findModuleForFile(file, project)
+      if (BundlerUtil.hasGemfile(module)) {
+        val gemfile = BundlerUtil.getGemfile(module)
+        val hasMissingGems = gemfile != null && BundlerGemInfrastructure.hasMissingGems(gemfile)
+        if (!hasMissingGems) {
+          return RubyGemExecutionContext.tryCreate(null, module, GEM_NAME)
         }
-        LOG.warn(processHandler.toString())
-        return processHandler
+      } else {
+        return RubyGemExecutionContext.tryCreate(sdk, module, GEM_NAME)
+      }
+      return null
     }
 
-    override fun createCommandLine(): GeneralCommandLine {
-        throw RuntimeException("not implemented.")
+    fun tryCreate(project: Project, file: VirtualFile): SorbetLspServerDescriptor? {
+      val sdk = RubySdkUtil.getFileSdk(project, file) ?: return null
+      val executionContext = createGemExecutionContext(sdk, project, file) ?: return null
+      return SorbetLspServerDescriptor(
+        executionContext, project, executionContext.module!!.rootManager.contentRoots
+      )
     }
+  }
 }
